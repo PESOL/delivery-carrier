2f067612a16ab029cb447c27022003d1a8f13c9c
acm1pt-colorado <luisadanjimenez@gmail.com>
2019-05-31 09:31:24 +0200

    ADD ASM carrier label

---
+++ delivery_carrier_label_asm/__manifest__.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) All Rights Reserved 2014 Akretion
+#    @author David BEAL <david.beal@akretion.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+
+{
+    'name': 'Delivery Carrier Label ASM',
+    'version': '10.0.1.0.0',
+    'author': "Akretion,"
+              "PESOL,"
+              "Odoo Community Association (OCA)",
+    'maintener': 'Akretion',
+    'category': 'Warehouse',
+    'summary': "GLS carrier label printing",
+    'depends': [
+        'base_delivery_carrier_label'
+    ],
+    'website': 'http://www.akretion.com/',
+    'data': [
+        'data/delivery_carrier.xml',
+        'data/sequence.xml',
+        'views/picking_view.xml',
+    ],
+    'demo': [
+        'demo/res.partner.csv',
+        'demo/company.xml',
+        'demo/product.xml',
+    ],
+    'external_dependencies': {
+        'python': [
+            'pycountry',
+            'unidecode',
+        ],
+    },
+    'license': 'AGPL-3',
+    'tests': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
